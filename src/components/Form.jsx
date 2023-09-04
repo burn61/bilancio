f8dc7246d31e698e601a04ed862a31f0b0a4818b
burn61 <nutella@posta.it>
2023-09-04 19:24:57 +0200

    aggiunti defaultValue a form da verificare

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,18 @@
-import { MDBInput, MDBRadio, MDBRow, MDBCol } from "mdb-react-ui-kit";
+//import { useState } from "react";
+import { MDBInput, MDBRadio, MDBBtn, MDBRow, MDBCol } from "mdb-react-ui-kit";
 
-function Form() {
+function Form(props) {
+
+  const dataForm = {id: 0, date: '', descr: '', amount: '', sign: ''}
+  
+  if (props) {
+    dataForm.id = props.id;
+    dataForm.date = props.date;
+    dataForm.descr = props.descr;
+    dataForm.sign = props.euro < 0 ? '-' : '+'
+    dataForm.amount = Math.abs(+props.euro).toLocaleString('it-IT')
+  } 
+
   function filterNum(str) {
     const re =
       /\$|@|#|~|`|\%|\*|\^|\&|\(|\)|\+|\=|\[|\-|\_|\]|\[|\}|\{|\;|\:|\'|\"|\<|\>|\?|\||\\|\!|\$|\./g;
@@ -9,7 +21,6 @@ function Form() {
   }
 
   function matchPattern(input) {
-    //return /^\d*\,?\d*$/.test(input)
     return /^\d*\,?\d?\d?$/.test(input)
   }
   
@@ -17,7 +28,7 @@ function Form() {
     const x = filterNum(e.target.value);
     if (!matchPattern(x)) {
       e.target.value = e.target.value.slice(0, -1);
-      return //e.target.value
+      return
     }
     if (x.includes(",")) {
       return
@@ -25,18 +36,33 @@ function Form() {
     e.target.value = (+x).toLocaleString("it-IT");
   };
 
+  function localStringToNumber(stringx) {
+    stringx = stringx.replace(/\./g, "");
+    stringx = stringx.replace(/\,/g, ".");
+    console.log(stringx)
+  }
+
+  const handleCancel = (e) =>{
+    //setDataForm(emptyForm)
+  }
+
   return (
     <>
       <MDBRow>
         <MDBCol>
-          <MDBInput id="date-mov" label="Data" type="date" />
+          <MDBInput 
+            id="date-mov" 
+            label="Data" 
+            type="date"
+            defaultValue={dataForm.date} 
+          />
         </MDBCol>
         <MDBCol>
           <MDBInput
             id="descr"
             label="Descrizione movimento"
             type="text"
-            onChange={(e) => console.log(e)}
+            defaultValue={null || dataForm.descr}
           />
         </MDBCol>
       </MDBRow>
@@ -44,16 +70,16 @@ function Form() {
       <MDBRow>
         <MDBCol>
           <MDBRadio
-            name="RadioPlus"
+            name="RadioSign"
             id="Entrata"
-            value="option1"
+            defaultChecked={true ? dataForm.euro>0 : false}
             label="Entrata"
             inline
           />
           <MDBRadio
-            name="RadioMinus"
+            name="RadioSign"
             id="Spesa"
-            value="option2"
+            defaultChecked={true ? dataForm.euro<0 : false}
             label="Spesa"
             inline
           />
@@ -64,8 +90,21 @@ function Form() {
             label="Importo"
             type="text"
             onChange={(e) => handleAmount(e)}
+            defaultValue={dataForm.euro!='' ? dataForm.euro : ''}
           />
         </MDBCol>
+      </MDBRow>
+      <MDBRow className='justify-content-end mx-2'>
+        <p className="w-auto p-3">
+          <MDBBtn rounded className="mx-2" color="secondary" size='sm' onClick={(e)=>handleCancel(e)}>
+            Annulla
+          </MDBBtn>
+        </p>
+        <p className="w-auto p-3">
+          <MDBBtn rounded className="mx-2" color="secondary" size='sm' onClick={(e)=>console.log("Cliccato")}>
+            Invio
+          </MDBBtn>
+        </p>
       </MDBRow>
     </>
   );
